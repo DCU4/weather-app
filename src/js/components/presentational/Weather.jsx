f6c884493f0c6d52b0b39bfaf3b48c466711187a
DCU4 <DylanJConnor4@gmail.com>
2019-06-09 13:05:32 -0400

    fix deg symbol

--- src/js/components/presentational/Weather.jsx
+++ src/js/components/presentational/Weather.jsx
@@ -6,18 +6,19 @@ export default class Weather extends Component {
   // }
 
   render() {
+    const deg = <span>&#176;F</span>;
     let temp = this.props.temperature;
     let city = this.props.city;
     let country = this.props.country;
     let description = this.props.description;
     let humidity = this.props.humidity;
     let error = this.props.error;
+    
+    
     return (
       <section className={this.props.class}>
         <p>{city ? city+"," : ""} {country}</p>
-        
-        
-        <p>{temp ? temp+"deg" : ""}</p>
+        <p>{temp}<span>{temp ? deg : ""}</span></p>
         <p>{humidity ? humidity+"%" : ""}</p>
         <p>{description}</p>
         <p className={error?'error' : ''}>{error? error : ''}</p>

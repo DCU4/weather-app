02e143d63361f87a7d3df68b55800151ce50568e
DCU4 <DylanJConnor4@gmail.com>
2019-06-09 16:21:32 -0400

    begin adding in svgs

--- src/js/components/presentational/Svg.jsx
+++ src/js/components/presentational/Svg.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+// import sun from '../svg/sun.svg';
 
 export default class Svg extends Component {
   // constructor(props) {
@@ -9,6 +10,8 @@ export default class Svg extends Component {
     return (
     <div className={this.props.class+"weather-svg"}>
       <h1>Sunny?</h1>
+      <img src={require('./svg/sun.svg')}/>
+      {sun}
       {
         //svgs will be here eventually
       }
